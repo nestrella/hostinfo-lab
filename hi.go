2d7ffab8f70c9f3ffc0acb8854ed260f5d94e714
Noel Star <noel.estrella@gmail.com>
2025-11-19 12:55:51 -0400

    test7

--- hi.go
+++ hi.go
@@ -19,8 +19,7 @@ func main() {
 			log.Fatal(err)
 		}
 	}
-	//Find default output IP and hostname cvbvcbvccvb
-
+	//Find default output IP and hostname
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
 		log.Fatal(err)
